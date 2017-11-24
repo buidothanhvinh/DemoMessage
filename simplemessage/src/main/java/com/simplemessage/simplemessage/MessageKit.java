c280e52602a7c76c53521fa96bfaaa74a660183c
Vinh <vinh@digifutura.com>
2017-11-24 14:57:45 +0700

    Add new library

---
+++ simplemessage/src/main/java/com/simplemessage/simplemessage/MessageKit.java
@@ -0,0 +1,11 @@
+package com.simplemessage.simplemessage;
+
+/**
+ * @author aviet 2017-11-24
+ */
+public class MessageKit {
+
+    public static String getSimpleMessage(String inputString) {
+        return "Simple Message: " + inputString;
+    }
+}
